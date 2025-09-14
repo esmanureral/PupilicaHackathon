bc8d1b6efe9c60e3273d1a85f8dc7d00a69820c1
Esmanur Eral <esmanur2eral@gmail.com>
2025-09-14 18:44:46 +0300

    Add ReminderSystemManager.kt

---
+++ app/src/main/java/com/esmanureral/pupilicahackathon/reminder/ReminderSystemManager.kt
@@ -0,0 +1,20 @@
+package com.esmanureral.pupilicahackathon.reminder
+
+import android.content.Context
+import com.esmanureral.pupilicahackathon.data.local.ReminderSharedPreferences
+import com.esmanureral.pupilicahackathon.notification.NotificationHelper
+
+class ReminderSystemManager(private val context: Context) {
+
+    fun initialize() {
+        setupNotificationChannel()
+    }
+
+    private fun setupNotificationChannel() {
+        try {
+            NotificationHelper.createReminderChannel(context)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
