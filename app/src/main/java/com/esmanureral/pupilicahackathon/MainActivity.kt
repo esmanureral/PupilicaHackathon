2436abb525c474286a83186accc327ee1dc7c17f
Esmanur Eral <esmanur2eral@gmail.com>
2025-09-15 17:11:59 +0300

    Add Onboarding

--- app/src/main/java/com/esmanureral/pupilicahackathon/MainActivity.kt
+++ app/src/main/java/com/esmanureral/pupilicahackathon/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.esmanureral.pupilicahackathon.data.local.OnboardingPreferences
 import com.esmanureral.pupilicahackathon.reminder.ReminderSystemManager
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         ReminderSystemManager(this).initialize()
+
+        val navController = (supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment)
+            .navController
+
+        navController.graph = navController.navInflater.inflate(R.navigation.nav_graph).apply {
+            setStartDestination(
+                if (OnboardingPreferences(this@MainActivity).isOnboardingCompleted)
+                    R.id.homeFragment
+                else
+                    R.id.onboardingFragment
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/esmanureral/pupilicahackathon/data/model/OnboardingPage.kt
@@ -0,0 +1,8 @@
+package com.esmanureral.pupilicahackathon.data.model
+
+data class OnboardingPage(
+    val animationRes: Int,
+    val title: String,
+    val description: String,
+    val backgroundColor: Int
+)
