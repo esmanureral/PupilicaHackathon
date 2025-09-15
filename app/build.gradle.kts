2f4dabc952f2b3f81905280d537ed4a9224679de
Esmanur Eral <esmanur2eral@gmail.com>
2025-09-15 17:02:38 +0300

    Add dependencies viewpager2 and lottie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,9 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.viewpager2)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.lottie)
 }
